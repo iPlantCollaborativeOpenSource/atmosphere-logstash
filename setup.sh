b44a247ac8fc9c061215ca8adcc04c2503a50657
Evan Briones <erbriones@gmail.com>
2015-06-19 12:19:35 -0700

    add notes about gnu readlink dependency

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 [ -d /etc/logstash ] && echo "Please remove the existing logstash directory from /etc" && exit
+# NOTE: expects readlink to be GNU readlink
 REPO_DIR=$(dirname `readlink -f $0`)
 ln -s $REPO_DIR /etc/logstash
